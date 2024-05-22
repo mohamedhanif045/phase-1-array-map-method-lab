c8ebd5612c44cf598d8d3765daf9aa42bcc97f95
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-22 10:27:21 +0300

    aray map

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => {
+        // Keep words that are fully uppercase as they are
+        if (word === word.toUpperCase()) {
+          return word;
+        } else {
+          return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        }
+      })
+      .join(' ');
+  });
 }
+
+console.log(titleCased());
+[
+  "What Does The This Keyword Mean?",
+  "What Is The Constructor OO Pattern?",
+  "Implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What Is NaN And How Can We Check For It",
+  "What Is The Difference Between StopPropagation And PreventDefault?",
+  "Immutable State And Pure Functions",
+  "What Is The Difference Between == And ===?",
+  "What Is The Difference Between Event Capturing And Bubbling?",
+  "What Is JSONP?"
+]
